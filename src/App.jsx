26df48cfdbf4752f8f3ae552d3e988a8f667a6a2
russbus5 <rlee5sbs@gmail.com>
2026-01-07 15:11:58 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Habit Tracker</h1>
+      <p>Google login and habits will go here!</p>
+    </div>
+  );
+}
